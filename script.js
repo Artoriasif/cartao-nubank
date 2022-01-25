a71ff006846ceb2c93abcf6bb1c93ec5c6f12bcb
Pedro <pedrohh0012@icloud.com>
2022-01-25 17:45:00 -0300

    Comentarios sobre o código

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ card.addEventListener("mouseenter", cardEnter);
 function cardEffect(event) {
   const cardWidth = card.offsetWidth;
   const cardHeight = card.offsetHeight;
-  const centerX = card.offsetLeft + cardWidth / 2;
+  const centerX = card.offsetLeft + cardWidth / 2; //para marcação do cursor do mouse no card
   const centerY = card.offsetTop + cardHeight / 2;
   const positionX = event.clientX - centerX;
   const positionY = event.clientY - centerY;
 
-  const rotateX = ((+1 * 25 * positionY) / (cardHeight / 2)).toFixed(2);
+  const rotateX = ((+1 * 25 * positionY) / (cardHeight / 2)).toFixed(2); //tofixed para limitar a casas decimais
   const rotateY = ((-1 * 25 * positionX) / (cardWidth / 2)).toFixed(2);
 
   console.log(rotateX, rotateY);
 
-  card.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  card.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`; //para melhorar o efeito 3D
 }
 
 function cardBack(event) {
@@ -27,7 +27,7 @@ function cardBack(event) {
 
 function cardTransition() {
   clearInterval(card.transitionId);
-  card.style.transition = "transform 400ms";
+  card.style.transition = "transform 400ms"; //Para a rotação nao ficar muito brusca
   card.transitionId = setTimeout(() => {
     card.style.transition = "";
   }, 400);
